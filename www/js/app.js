8c9d77faba279cb2618fde5f4b1d78c75fa0a45e
SolomonT <thanhvt1609@gmail.com>
2016-02-17 22:49:44 +0700

    fix basic layout

--- www/js/app.js
+++ www/js/app.js
@@ -1,10 +1,19 @@
 var app = new Framework7({
-    material: true
+    material: true,
+    cache: false,
+    modalTitle: 'madDiscovery',
+    onAjaxStart: function (xhr) {
+        app.showIndicator();
+    },
+    onAjaxComplete: function (xhr) {
+        app.hideIndicator();
+    }
 });
 
 var $$ = Dom7;
 
-var mainView = app.addView('.view-main', {
+var mainView = app.addView('.view-main', {});
 
+app.onPageInit('event-form', function (page) {
+    $$('div.list-block').removeClass('inputs-list');
 });
-
