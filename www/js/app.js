e7478e582e616814af13b7f5df909041715c5a20
Tien Thanh <thanhvt1609@gmail.com>
2016-03-27 10:34:14 +0700

    restructure, prepare for testing on real phone

--- www/js/app.js
+++ www/js/app.js
@@ -1,15 +1,143 @@
-var app = new Framework7({
-    material: true,
-    cache: false,
-    modalTitle: 'madDiscovery',
-    precompileTemplates: true,
-    onAjaxStart: function (xhr) {
-        app.showIndicator();
-    },
-    onAjaxComplete: function (xhr) {
-        app.hideIndicator();
-    }
-});
+var app;
+var mainView;
+var data;
+
+function init(){
+    app = new Framework7({
+        material: true,
+        cache: false,
+        modalTitle: 'madDiscovery',
+        precompileTemplates: true,
+        onAjaxStart: function () {
+            app.showIndicator();
+        },
+        onAjaxComplete: function () {
+            app.hideIndicator();
+        }
+    });
+
+    mainView = app.addView('.view-main', {});
+    var conn = null;
+    initDb(function (connection) {
+        conn = connection;
+        if (!conn) {
+            alert("CANNOT CONNECT DATABASE");
+        }
+    });
+
+    data = {
+        events: [],
+        action: null,
+        event: null,
+        map: null
+    };
+
+    listEvent(conn, function (events) {
+        data.events = data.events.concat(events);
+        loadEventList(data, function (error) {
+            if (error) {
+                alert(error);
+            }
+        });
+    });
+
+    app.onPageInit('index', function () {
+        $('a#event-add').click(function (e) {
+            e.preventDefault();
+            data.action = 'Add';
+            loadEventForm(data, function (error) {
+                if (error) {
+                    alert(error);
+                }
+            });
+        });
+
+        $('div.card-header a').on('click', function (e) {
+            e.preventDefault();
+            var id = $(this).attr('id');
+            data.action = 'Edit';
+            getEvent(conn, id, function (event) {
+                data.event = event;
+                loadEventForm(data, function (error) {
+                    if (!error) {
+                        app.formFromJSON('#event-form', data.event);
+                    } else {
+                        data.event = null;
+                        alert(error);
+                    }
+                });
+            });
+        });
+    });
+
+    app.onPageInit('map', function () {
+        $('#map-done').on('click', function (e) {
+            e.preventDefault();
+            mainView.router.back();
+            if (data.map) {
+                $('#event-location').val(data.map.formatted_address);
+                $('#event-lat').val(data.map.geometry.location.lat());
+                $('#event-lng').val(data.map.geometry.location.lng());
+            }
+        });
+    });
+
+
+    app.onPageInit('event-form', function () {
+        $('div.list-block').removeClass('inputs-list');
+        app.calendar({
+            input: '#event-date'
+        });
+        $('a#load-map').on('click', function (e) {
+            e.preventDefault();
+            loadMap(data);
+        });
+        $('#save-event').on('click', function () {
+            var event = app.formToJSON('#event-form');
+            validateEvent(event, function (err) {
+                if (err.length > 0) {
+                    err.forEach(function (value) {
+                        sendNotify(value);
+                    });
+
+                }
+                else {
+                    if (event.id == "") {
+                        insertEvent(conn, event, function (d) {
+                            data.events.push(d);
+
+                            reloadEventList(data, function (error) {
+                                if (!error) {
+                                    sendNotify("New event created");
+                                } else {
+                                    alert(error);
+                                }
+                            });
+                        });
+                    } else {
+                        updateEvent(conn, event, function (r) {
+                            data.events.forEach(function (v, k) {
+                                if (v.id == r.id) {
+                                    data.events[k] = r;
+                                }
+                            });
+
+                            reloadEventList(data, function (error) {
+                                if (!error) {
+                                    sendNotify("Event Updated");
+                                    data.event = null;
+                                } else {
+                                    alert(error);
+                                }
+                            });
+                        });
+                    }
+                }
+            });
+
+        });
+    });
+}
 
 function sendNotify(message) {
     app.addNotification({
@@ -18,23 +146,6 @@ function sendNotify(message) {
         message: message
     });
 }
-
-var mainView = app.addView('.view-main', {});
-
-var conn = null;
-initDb(function (connection) {
-    conn = connection;
-    if (!conn) {
-        alert("CANNOT CONNECT DATABASE");
-    }
-});
-
-var data = {
-    events: [],
-    action: null,
-    event: null,
-    map: null
-};
 
 function loadTemplate(template, context, callback) {
     $.get('templates/' + template + '.html')
@@ -50,7 +161,7 @@ function loadTemplate(template, context, callback) {
 
 function loadEventList(context, callback) {
     loadTemplate('event/index', context, function (content) {
-        if (content == "") {
+        if (content == '') {
             callback("Cannot load event list!");
         } else {
             mainView.router.loadContent(content);
@@ -61,7 +172,7 @@ function loadEventList(context, callback) {
 
 function reloadEventList(context, callback) {
     loadTemplate('event/index', context, function (content) {
-        if (content == "") {
+        if (content == '') {
             callback("Cannot load event list!");
         } else {
             mainView.router.reloadContent(content);
@@ -72,7 +183,7 @@ function reloadEventList(context, callback) {
 
 function loadEventForm(context, callback) {
     loadTemplate('event/form', context, function (content) {
-        if (content == "") {
+        if (content == '') {
             callback("Cannot load event form!");
         } else {
             mainView.router.loadContent(content);
@@ -83,7 +194,7 @@ function loadEventForm(context, callback) {
 
 function loadMap(context, callback) {
     loadTemplate('event/map', context, function (content) {
-        if (content == "") {
+        if (content == '') {
             callback("Cannot load map!");
         } else {
             mainView.router.loadContent(content);
@@ -122,110 +233,3 @@ function loadMap(context, callback) {
         }
     });
 }
-
-listEvent(conn, function (events) {
-    data.events = data.events.concat(events);
-    loadEventList(data, function (error) {
-        if (error) {
-            alert(error);
-        }
-    });
-});
-
-app.onPageInit('index', function () {
-    $('a#event-add').click(function (e) {
-        e.preventDefault();
-        data.action = 'Add';
-        loadEventForm(data, function (error) {
-            if (error) {
-                alert(error);
-            }
-        });
-    });
-
-    $('div.card-header a').on('click', function (e) {
-        e.preventDefault();
-        var id = $(this).attr('id');
-        data.action = 'Edit';
-        getEvent(conn, id, function (event) {
-            data.event = event;
-            loadEventForm(data, function (error) {
-                if (!error) {
-                    app.formFromJSON('#event-form', data.event);
-                } else {
-                    data.event = null;
-                    alert(error);
-                }
-            });
-        });
-    });
-});
-
-app.onPageInit('map', function () {
-    $('#map-done').on('click', function (e) {
-        e.preventDefault();
-        mainView.router.back();
-        if (data.map) {
-            $('#event-location').val(data.map.formatted_address);
-            $('#event-lat').val(data.map.geometry.location.lat());
-            $('#event-lng').val(data.map.geometry.location.lng());
-        }
-    });
-});
-
-
-app.onPageInit('event-form', function () {
-    $('div.list-block').removeClass('inputs-list');
-    app.calendar({
-        input: '#event-date'
-    });
-    $('a#load-map').on('click', function (e) {
-        e.preventDefault();
-        loadMap(data);
-    });
-    $('#save-event').on('click', function () {
-        var event = app.formToJSON('#event-form');
-        validateEvent(event, function (err) {
-            if (err.length > 0) {
-                err.forEach(function (value) {
-                    sendNotify(value);
-                });
-
-            }
-            else {
-                if (event.id == "") {
-                    insertEvent(conn, event, function (d) {
-                        data.events.push(d);
-                        
-                        reloadEventList(data, function (error) {
-                            if (!error) {
-                                sendNotify("New event created");
-                            } else {
-                                alert(error);
-                            }
-                        });
-
-                    });
-                } else {
-                    updateEvent(conn, event, function (r) {
-                        data.events.forEach(function (v, k) {
-                            if (v.id == r.id) {
-                                data.events[k] = r;
-                            }
-                        });
-
-                        reloadEventList(data, function (error) {
-                            if (!error) {
-                                sendNotify("Event Updated");
-                                data.event = null;
-                            } else {
-                                alert(error);
-                            }
-                        });
-                    });
-                }
-            }
-        });
-
-    });
-});
